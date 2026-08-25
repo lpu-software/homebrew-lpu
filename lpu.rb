3eada306f3ce7236e3f6fbde8044c1988207dee5
Yatish Kumar <143833699+Yatishydv@users.noreply.github.com>
2026-08-26 01:25:26 +0530

    Update homepage and URL for LPU software

--- lpu.rb
+++ lpu.rb
@@ -1,7 +1,7 @@
 class Lpu < Formula
   desc "LPU - Remote Terminal Management"
-  homepage "https://github.com/yatishydv/rtm"
-  url "https://github.com/yatishydv/rtm/releases/download/v1.0.0/lpu-mac.tar.gz"
+  homepage "https://github.com/lpu-software/rtm"
+  url "https://github.com/lpu-software/rtm/releases/download/v1.0.0/lpu-mac.tar.gz"
   sha256 "3e242542ec0e4b37ee2967e369192b9713ba349f35c6feb469b65d153fcd91af"
   version "1.0.0"
 
