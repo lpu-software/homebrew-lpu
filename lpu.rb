c70c3039514a5a82dafe397748d4e7bff61cb6f3
Yatish Kumar <143833699+Yatishydv@users.noreply.github.com>
2026-08-28 01:11:05 +0530

    Update LPU version to 1.0.20 and SHA256

--- lpu.rb
+++ lpu.rb
@@ -1,9 +1,9 @@
 class Lpu < Formula
   desc "LPU - Remote Terminal Management"
   homepage "https://github.com/lpu-software/rtm"
-  url "https://github.com/lpu-software/rtm/releases/download/v1.0.16/lpu-mac.tar.gz"
-  sha256 "1fbaba2db6cb6a56e92516de09ae2c5397c54e0d9ac1e041ca56d9c8626d492b"
-  version "1.0.16"
+  url "https://github.com/lpu-software/rtm/releases/download/v1.0.20/lpu-mac.tar.gz"
+  sha256 "b65f8fc916940407483aa4a30cc5d0a6ab5399970922c0a7282d6e5430b27f8e"
+  version "1.0.20"
 
   def install
     # Install the binary
