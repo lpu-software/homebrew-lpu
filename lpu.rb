0590d871fa8229ec804a83dff319dc5f47818dc4
Yatish Kumar <143833699+Yatishydv@users.noreply.github.com>
2026-09-06 06:40:33 +0530

    Update LPU version to 1.0.32

--- lpu.rb
+++ lpu.rb
@@ -1,9 +1,9 @@
 class Lpu < Formula
   desc "LPU - Remote Terminal Management"
   homepage "https://github.com/lpu-software/rtm"
-  url "https://github.com/lpu-software/rtm/releases/download/v1.0.31/lpu-mac.tar.gz"
-  sha256 "b99009b2c8579c8d42452b2aab94c9574605a559e18960261074d8c47bb70fbd"
-  version "1.0.31"
+  url "https://github.com/lpu-software/rtm/releases/download/v1.0.32/lpu-mac.tar.gz"
+  sha256 "89e9a9f142681e6756bb327fa7cfe594146ceda4041d7a0a049d927ab5eb10d6"
+  version "1.0.32"
 
   def install
     # Install the binary
